b3864c07687e03fd2049fa27a4a8fd3c2edb00f6
ali7med <ali.hameed.it@gmail.com>
2024-02-12 12:02:33 +0300

    add fontIcon , WangEditor , make index archive card

---
+++ src/components/WangEditor.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref, shallowRef } from "vue";
+import "@wangeditor/editor/dist/css/style.css"; // import css
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+const editorRef = shallowRef();
+import { i18nChangeLanguage } from "@wangeditor/editor";
+const modelValue = defineModel<string>();
+i18nChangeLanguage("en");
+
+// content HTML
+const valueHtml = ref("<p>hello</p>");
+const toolbarConfig = {};
+const editorConfig = { placeholder: "Type here..." };
+const handleCreated = (editor: string) => {
+  editorRef.value = editor; // record editor instance
+};
+</script>
+
+<template>
+  <div style="border: 1px solid #ccc; margin-top: 10px">
+    <Toolbar
+      :editor="editorRef"
+      :defaultConfig="toolbarConfig"
+      mode="default"
+      style="border-bottom: 1px solid #ccc"
+    />
+    <Editor
+      :defaultConfig="editorConfig"
+      mode="default"
+      v-model="modelValue"
+      style="height: 400px; overflow-y: hidden; text-align: right"
+      @onCreated="handleCreated"
+      readonly="false"
+    />
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -6,41 +6,10 @@ import "./assets/main.css";
 import "./assets/vue-select.css";
 import "./assets/print.css";
 import vSelect from "vue-select";
-import "nprogress/nprogress.css";
-
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
 import { MotionPlugin } from "@vueuse/motion";
-import { QuillEditor } from "@vueup/vue-quill";
-const globalOptions = {
-  modules: {
-    toolbar: [
-      "bold",
-      "italic",
-      "underline",
-      { align: "right" },
-      { direction: "rtl" },
-      "blockquote",
-      "code-block",
-      { list: "ordered" },
-      { list: "bullet" },
-    ],
-  },
-  placeholder: "قم بملئ الملاحظات",
-  readOnly: false,
-  direction: "rtl",
-  formats: ["direction", "rtl"],
-};
-// set default globalOptions prop
-QuillEditor.props.globalOptions.default = () => globalOptions;
-
-// register QuillEditor component
-
-//import preset
 
 const app = createApp(App);
 app.component("vSelect", vSelect);
-app.component("QuillEditor", QuillEditor);
-
 app.use(createPinia());
 app.use(MotionPlugin);
 app.use(router);
@@ -51,13 +20,5 @@ app.directive("focus", {
     el.focus();
   },
 });
-//#region Permission
 
-// app.config.globalProperties.$baseURL =
-//   process.env.NODE_ENV === "production" ? "/10/" : "/";
-//#region Permission Globally
-
-// console.log(can("admin"));
-//#endregion
-//#endregion
 app.mount("#app");
