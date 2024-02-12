e4b44a36de1e7183dd61de7a13d0d40fe97441d7
Zaid Kindman <zaid1997uk@gmail.com>
2024-02-12 09:30:24 +0300

    modify IPage components

--- src/components/ihec/IPage.vue
+++ src/components/ihec/IPage.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="container">
+  <div class="container mx-auto px-10 py-5">
     <slot name="header"></slot>
-    <slot></slot>
+    <div class="bg-slate-50 dark:bg-slate-800">
+      <slot name="content"></slot>
+    </div>
     <slot name="footer"></slot>
   </div>
 </template>
