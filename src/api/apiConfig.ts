26f09ebfbcafc1981dc44217d1ee122bc1d98f6c
Zaid Kindman <zaid1997uk@gmail.com>
2024-02-14 11:30:42 +0300

    change default backend URL

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import router from "@/router";
 import { useAuthStore } from "@/stores/authStore";
-import envConfig from "./envConfig";  
+import envConfig from "./envConfig";
 const Api = axios.create({
   //baseURL: "http://10.9.8.7/workflow_ihec/public/api",
   // baseURL: "http://10.10.10.10/workflow_ihec/public/api",
