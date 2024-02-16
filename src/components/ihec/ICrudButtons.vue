a6b0405c6637163482fab089c4758ee3996ac9dc
sameerFront <neutron.02f@gmail.com>
2024-02-16 17:58:31 +0300

    on form component

---
+++ src/components/ihec/ICrudButtons.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { t } from "@/utils/I18nPlugin";
+
+const props = defineProps({
+  type: {
+    type: Array,
+    default: () => [],
+  },
+  onClick: {
+    type: Function,
+  },
+});
+const hasType = (buttonType: string): boolean => {
+  return props.type.includes(buttonType);
+};
+</script>
+
+<template>
+  <div
+    class="max-w-screen-xl flex flex-wrap flex-row-reverse justify-between p-4"
+  >
+    <IButton
+      v-if="hasType('add')"
+      :text="t('Create')"
+      :onClick="props.onClick"
+    />
+    <IButton
+      v-if="hasType('update')"
+      :text="t('Update')"
+      :onClick="props.onClick"
+    />
+    <IButton
+      v-if="hasType('delete')"
+      color="red"
+      :text="t('Delete')"
+      :onClick="props.onClick"
+    />
+  </div>
+</template>
---
+++ src/components/ihec/IFullRow.vue
@@ -0,0 +1,18 @@
+<!-- <script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  col: {
+    type: [Number],
+    default: 12,
+  },
+});
+//${props.col}
+var rowClass = `grid grid-cols-12`;
+</script> -->
+
+<template>
+  <div class="grid grid-cols-12">
+    <slot></slot>
+  </div>
+</template>
