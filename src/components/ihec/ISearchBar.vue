42b9f147d85aee7a83e533c77942c636cefd8e18
sameerFront <neutron.02f@gmail.com>
2024-02-16 21:32:06 +0300

    on form component

---
+++ src/components/ihec/ISearchBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { t } from "@/utils/I18nPlugin";
+const props = defineProps({
+  getDataButton: {
+    type: Function,
+  },
+});
+</script>
+<template>
+  <slot></slot>
+
+  <!-- search button -->
+  <div class="ml-4 lg:mt-0 xs:mt-2">
+    <IButton
+      color="green"
+      type="outlined"
+      :text="t('Search')"
+      :onClick="props.getDataButton"
+    />
+  </div>
+</template>
