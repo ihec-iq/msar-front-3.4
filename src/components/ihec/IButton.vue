2709fa053e89bba371fb896b0cb1a9761a97d77c
Zaid Kindman <zaid1997uk@gmail.com>
2024-02-18 11:54:51 +0300

    Update IButton.vue

--- src/components/ihec/IButton.vue
+++ src/components/ihec/IButton.vue
@@ -29,7 +29,7 @@ const props = defineProps({
 });
 
 const defaultClass = `bg-${props.color}-500 hover:bg-${props.color}-700 duration-500 text-white font-bold w-${props.width} py-2 px-4 rounded`;
-const outlinedClass = `border border-${props.color}-500 text-${props.color}-500 hover:bg-${props.color}-500 hover:text-${props.color}-700 duration-500 font-bold w-${props.width} py-2 px-4 rounded`;
+const outlinedClass = `ring-1 ring-${props.color}-700 text-${props.color}-500 hover:bg-${props.color}-200 dark:hover:bg-${props.color}-500 dark:hover:text-white duration-300 font-bold w-${props.width} py-2 px-4 rounded`;
 const iconClass = `bg-${props.color}-500 hover:bg-${props.color}-700 text-white font-bold py-2 px-4 rounded inline-flex items-center`;
 
 var buttonClass =
