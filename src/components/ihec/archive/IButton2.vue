795af040057d4f335187ea156849d7f8487498ca
ali7med <ali.hameed.it@gmail.com>
2024-02-20 09:23:13 +0300

    custom Button2

--- src/components/ihec/archive/IButton2.vue
+++ src/components/ihec/archive/IButton2.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { Icon } from "@iconify/vue";
+import { computed, ref, watch, type PropType } from "vue";
 import { defineProps } from "vue";
 
 const props = defineProps({
@@ -13,14 +14,18 @@ const props = defineProps({
     default: "Btn",
   },
   type: {
-    type: String,
+    type: String as PropType<"default" | "outlined">,
     default: "default",
   },
   color: {
     type: String,
     default: "blue",
   },
-  icon: {
+  preIcon: {
+    type: String,
+    default: "",
+  },
+  postIcon: {
     type: String,
     default: "",
   },
@@ -33,47 +38,82 @@ const props = defineProps({
     default: "32",
   },
 });
-
-var defaultClass = `bg-${props.color}-500 hover:bg-${props.color}-700 duration-500 text-white font-bold w-${props.width} py-2 px-4 rounded`;
-
-var outlinedClass = `border border-${props.color}-500 text-${props.color}-500 hover:bg-${props.color}-500 hover:text-${props.color}-700 duration-500 font-bold w-${props.width} py-2 px-4 rounded`;
-
-var iconClass = `bg-${props.color}-500 hover:bg-${props.color}-700 text-white font-bold py-2 px-4 rounded inline-flex items-center`;
-
-var buttonClass =
-  props.type === "default"
-    ? defaultClass
-    : props.type === "outlined"
-    ? outlinedClass
-    : defaultClass;
-
-if (props.icon != "") {
-  buttonClass = iconClass;
-}
-watch(
-  () => props.color,
-  (newQuestion) => {
-    console.log(newQuestion);
-
-    defaultClass = `bg-${newQuestion}-500 hover:bg-${newQuestion}-700 duration-500 text-white font-bold w-${props.width} py-2 px-4 rounded`;
-
-    outlinedClass = `border border-${newQuestion}-500 text-${newQuestion}-500 hover:bg-${newQuestion}-500 hover:text-${newQuestion}-700 duration-500 font-bold w-${newQuestion.width} py-2 px-4 rounded`;
-
-    iconClass = `bg-${newQuestion}-500 hover:bg-${newQuestion}-700 text-white font-bold py-2 px-4 rounded inline-flex items-center`;
+const PreIcon = computed(() => {
+  return "mdi:" + props.preIcon;
+});
+const PostIcon = computed(() => {
+  return "mdi:" + props.postIcon;
+});
+const classIcon = computed(() => {
+  return (
+    "w-5 h-5 text-" +
+    props.color +
+    "-500 dark:text-" +
+    props.color +
+    "-400 mr-2 ml-2 basis-1/3 "
+  );
+});
+const buttonClass = computed(() => {
+  let colorClass = "";
+  if (props.type === "default") {
+    colorClass =
+      "bg-" +
+      props.color +
+      "-500 hover:bg-" +
+      props.color +
+      "-700 duration-500 text-white font-bold w-" +
+      props.width +
+      " py-2 px-4 rounded";
+  } else if (props.type === "outlined") {
+    colorClass =
+      "ring-1 ring-" +
+      props.color +
+      "-700 text-" +
+      props.color +
+      "-500 hover:bg-" +
+      props.color +
+      "-200 dark:hover:bg-" +
+      props.color +
+      "500 dark:hover:text-white duration-300 font-bold w-" +
+      props.width +
+      " py-2 px-4 rounded";
+  } else {
+    colorClass =
+      "bg-" +
+      props.color +
+      "-500 hover:bg-" +
+      props.color +
+      "-700 duration-500 text-white font-bold w-" +
+      props.width +
+      " py-2 px-4 rounded";
   }
-);
+  return colorClass;
+});
 </script>
 
 <template>
-  <button @click="onClick()" :class="buttonClass">
-    <svg
-      v-if="props.icon != ''"
-      class="fill-current w-4 h-4 mx-2"
-      :xmlns="props.icon"
-      viewBox="0 0 20 20"
-    >
-      <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-    </svg>
-    <span>{{ props.text }}</span>
+  <button
+    @click="onClick()"
+    :class="buttonClass"
+    class="flex flex-row mx-2 mt-3"
+  >
+    <Icon
+      v-if="props.preIcon != ''"
+      :icon="PreIcon"
+      :class="classIcon"
+      class="grow-0"
+    />
+    <span class="grow">{{ props.text }}</span>
+    <Icon
+      v-if="props.postIcon != ''"
+      :icon="PostIcon"
+      :class="classIcon"
+      class="grow-0"
+    />
   </button>
 </template>
+<style scoped>
+button:hover {
+  @apply transform scale-105;
+}
+</style>
