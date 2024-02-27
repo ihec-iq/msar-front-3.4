623488da8a03a71a972e8b2911be79677d792051
ali7med <ali.hameed.it@gmail.com>
2024-02-27 11:12:05 +0300

    input voucher part2

--- src/components/ihec/IRow.vue
+++ src/components/ihec/IRow.vue
@@ -6,17 +6,25 @@ const props = defineProps({
     type: [Number, String],
     default: 1,
   },
+  colXl: {
+    type: [Number, String],
+    default: null,
+  },
   colLg: {
     type: [Number, String],
-    default: 1,
+    default: null,
   },
   colMd: {
     type: [Number, String],
-    default: 1,
+    default: null,
   },
   colSm: {
     type: [Number, String],
-    default: 1,
+    default: null,
+  },
+  colXs: {
+    type: [Number, String],
+    default: null,
   },
   debug: {
     type: Boolean,
@@ -24,14 +32,21 @@ const props = defineProps({
   },
 });
 const colClass = computed(() => {
-  return `grid grid-cols-${props.col} lg:grid-cols-${props.colLg} md:grid-cols-${props.colMd} sm:grid-cols-${props.colSm}`;
+  var classStyle ="grid "
+  if (props.col != null) classStyle += ` grid-cols-${props.col}`;
+  if (props.colXl != null) classStyle += ` xl:grid-cols-${props.colXl}`;
+  if (props.colLg != null) classStyle += ` lg:grid-cols-${props.colLg}`;
+  if (props.colMd != null) classStyle += ` md:grid-cols-${props.colMd}`;
+  if (props.colSm != null) classStyle += ` sm:grid-cols-${props.colSm}`;
+  if (props.colXs != null) classStyle += ` xs:grid-cols-${props.colXs}`;
+  return classStyle;
 });
 const debugClass = computed(() => {
   if (props.debug) return `border border-1 border-red-500`;
   return "";
 });
 
-// var colClass  = `ltr:mr-2 rtl:ml-2 col-span-${props.col}`;
+// var colClass  = `ltr:mr-2 rtl:ml-2 grid-cols-${props.col}`;
 </script>
 <template>
   <div
---
+++ src/components/ihec/test/IRow2.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    span: {
+        type: Number,
+        required: true,
+    },
+    spanLg: {
+        type: Number,
+        default: null,
+    },
+    spanMd: {
+        type: Number,
+        default: null,
+    },
+    spanSm: {
+        type: Number,
+        default: null,
+    },
+    
+});
+
+function getClassNames(): string {
+    let classNames = `grid grid-cols-${props.span}`;
+    if (props.spanLg !== null) {
+        classNames += ` lg:grid-cols-${props.spanLg}`;
+    }
+    if (props.spanMd !== null) {
+        classNames += ` md:grid-cols-${props.spanMd}`;
+    }
+    if (props.spanSm !== null) {
+        classNames += ` sm:grid-cols-${props.spanSm}`;
+    }
+    return classNames;
+}
+</script>
+
+<template>
+    <div :class="getClassNames()">
+        <slot></slot> 
+    </div>
+</template>
