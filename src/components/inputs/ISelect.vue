f1786667b7fd85375c06308fc8832ec88c1b1b0a
Zaid Kindman <zaid1997uk@gmail.com>
2024-03-24 09:22:25 +0300

    Add check validation to "ISelect" component

--- src/components/inputs/ISelect.vue
+++ src/components/inputs/ISelect.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 const modelValue = defineModel<any>();
 const emits = defineEmits<{
   //(e: "change", id: number): void;
   (e: "onChange"): void;
 }>();
-defineProps({
+const props = defineProps({
   name: {
     type: String,
     required: true,
@@ -27,6 +29,18 @@ const Change = () => {
 };
 // import { useI18n } from "@/stores/i18n/useI18n";
 // const { t } = useI18n();
+
+const inputClasses = ref("_input");
+
+const checkRequired = () => {
+  if (!props.IsRequire) return;
+
+  if (modelValue.value) {
+    inputClasses.value = "_input";
+  } else {
+    inputClasses.value = "_input border border-red-500";
+  }
+};
 </script>
 <template>
   <div class="mb-2 mx-1">
@@ -34,10 +48,11 @@ const Change = () => {
       <span v-if="IsRequire" class="text-red-600">*</span> {{ label }}
     </label>
     <select
-      class="_input"
+      :class="inputClasses"
       :name="name"
       v-model="modelValue"
       @change="Change"
+      @focusout="checkRequired"
     >
       <option
         v-for="(option, index) in options"
