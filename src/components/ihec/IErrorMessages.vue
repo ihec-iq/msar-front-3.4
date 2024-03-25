7f69bba20a7784c8aa68ff575b03aa9a5e62b9ba
Zaid Kindman <zaid1997uk@gmail.com>
2024-03-25 12:59:09 +0300

    Create IErrorMessages.vue

---
+++ src/components/ihec/IErrorMessages.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  validationResult: {
+    type: Object,
+    default: () => {
+      success: false;
+      errors: [];
+    },
+  },
+});
+</script>
+<template>
+  <div
+    class="w-full border border-red-500 mx-auto rtl:text-right ltr:text-left p-5 rounded-lg"
+    v-if="!validationResult.success"
+  >
+    <p class="font-semibold">الاخطاء التي يجب معالجتها</p>
+    <ul v-for="(error, index) in validationResult.errors" :key="index">
+      <h2 class="text-xl font-bold text-black py-2">
+        {{ error.fieldName }}
+      </h2>
+      <li
+        v-for="(message, index) in error.messages"
+        :key="index"
+        class="text-lg font-bold text-red-500"
+      >
+        {{ `- ${message}` }}
+      </li>
+    </ul>
+  </div>
+</template>
