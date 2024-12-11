086b3be223a3d7401ae119b7a2cd6abaa3e7efa7
Ali Hameed Al-Hashime <ALi.hameed.it@gmail.com>
2024-12-11 19:12:20 +0300

    Update authMiddleware.ts

--- src/router/middleware/authMiddleware.ts
+++ src/router/middleware/authMiddleware.ts
@@ -8,14 +8,7 @@ export default async function authMiddleware(
   next: NavigationGuardNext
 ) {
   // Perform your authentication logic here
-  const token =await useAuthStore().getSecureToken();
-  const isAuthenticated = token != "" && token != undefined ? true : false; // Replace with your authentication check
-  if (!isAuthenticated) {
-    // If not authenticated, redirect to login
-    next("/login");
-    return;
-  }
-  // If authenticated, proceed to the next middleware
+  await useAuthStore().getSecureToken();
   next();
   return;
 }
