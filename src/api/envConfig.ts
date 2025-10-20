b59f9d274b5acd411807a13531b41fb9149088a2
Ali Hameed Al-Hashime <ALi.hameed.it@gmail.com>
2025-10-20 10:35:42 +0300

    20251020 fix online push

--- src/api/envConfig.ts
+++ src/api/envConfig.ts
@@ -1,7 +1,5 @@
 export default {
   //_baseURL: "http://10.10.10.11/msar-backend-12/public/api",
-  _baseURL: "http://localhost/msar-backend-12/public/api",
-  //_baseURL: "https://ca68-109-205-117-128.ngrok-free.app/ihec-backend/public/api",
-  //_baseURL: "http://localhost/ihec-backend/public/api",
-  //baseURL: "http://localhost/workflow_ihec/public/api",
+  _baseURL: "https://ihec.hlm.one/api",
+  //_baseURL: "http://localhost/msar-backend-12/public/api", 
 };
